84d66a02e1cfd76c6dba5bcdce69663b19f8204a
Subhash Yadav <90600558+websubhash@users.noreply.github.com>
2021-12-07 16:07:36 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,5 @@ module.exports = {
   transform: {
     "^.+\\.ts$": "<rootDir>/node_modules/ts-jest/preprocessor.js",
   },
-//   testMatch: ["<rootDir>/tests/**/*.test.ts"],
+  testMatch: ["<rootDir>/tests/**/*.test.ts"],
 };
